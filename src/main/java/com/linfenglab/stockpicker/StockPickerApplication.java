849c61b42e1c15207596764e2a34201dc4779baa
Linfeng <linfenglee321@gmail.com>
2018-06-09 16:47:47 -0400

    initial commit

---
+++ src/main/java/com/linfenglab/stockpicker/StockPickerApplication.java
@@ -0,0 +1,12 @@
+package com.linfenglab.stockpicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPickerApplication.class, args);
+	}
+}
